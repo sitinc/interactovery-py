ab971c2dd4ef81a654027e2164836c4bf15004ee
justin-oos <justin@outofservice.org>
2024-01-14 15:14:37 -0500

    Bump the version to 0.0.6.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     # Basic package information:
     name="interactovery",
-    version="0.0.5",
+    version="0.0.6",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
 
